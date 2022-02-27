930810ceca89f417ac5b113ce86e07ce307c73a0
dav <davidvardanyan94@gmail.com>
2022-02-27 17:43:32 +0400

    wrote first working tests

---
+++ tests/login.spec.js
@@ -0,0 +1,35 @@
+const { test, expect } = require("@playwright/test");
+
+test.describe("Login tests", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:3000/");
+  });
+
+  test("Login with valid credentials", async ({ page }) => {
+    await page.locator("text='Login'").click();
+    await page.locator('#userName').fill("davit.vardanyan@betconstruct.com");
+    await page.locator('#password').fill("Test1234");
+    await page.locator("text='sign_in'").click();
+    await expect(page.locator("[data-testid='balance-currency']")).toContainText('AMD')
+});
+
+test("Login with invalid username and valid password", async ({ page }) => {
+    await page.locator("text='Login'").click();
+    await page.locator('#userName').fill("davit.vardanyan@gmail.com");
+    await page.locator('#password').fill("Test1234");
+    await page.locator("text='sign_in'").click();
+    await expect(page.locator(".danger > p")).toContainText('Invalid Username and/or password')
+});
+
+test("Login with valid username and invalid password", async ({ page }) => {
+    await page.locator("text='Login'").click();
+    await page.locator('#userName').fill("davit.vardanyan@betconstruct.com");
+    await page.locator('#password').fill("Test12345");
+    await page.locator("text='sign_in'").click();
+    await expect(page.locator(".danger > p")).toContainText('Invalid Username and/or password')
+});
+  test.afterEach(async ({ page }) => {
+    await page.waitForTimeout(5000);
+    await page.close();
+  });
+});
